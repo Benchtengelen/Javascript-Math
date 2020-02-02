ed317eb72a6cbd728b81608a93580d4fcdfe5cb7
Feyyaz Yılmaz <45435495+Benchtengelen@users.noreply.github.com>
2020-02-02 15:24:13 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <title></title>
+</head>
+<body>
+   <h1> Javascript </h1>
+   <script>
+
+   document.write(Math.floor(1-9)*Math.random());
+   document.write(Math.PI+ "<br>");
+   document.write(Math.ceil(3.7));
+   document.write(Math.min(x=2,y=3,z=4));
+   document.write(Math.max(x=2,y=3,z=4));
+
+</script>
+</body>
+</html>
